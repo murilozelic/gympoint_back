6ded1eab50ae195462895cdf997fb496fe4374e8
Murilo Zelic <murilozelic@gmail.com>
2020-03-24 22:28:55 -0300

    SessionController agora retorna user completo

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -19,7 +19,9 @@ class SessionController {
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
@@ -32,9 +34,11 @@ class SessionController {
     const { id, name } = user;
 
     return res.json({
-      id,
-      name,
-      email,
+      user: {
+        id,
+        name,
+        email,
+      },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
       }),
